955fa2e5bde46bd177deb5186914e2335368c4cd
feven <feventsegay6@gmail.com>
2019-05-03 03:34:45 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,79 @@
+require "pry"
 def get_first_name_of_season_winner(data, season)
-  # code here
+ data[season].each do |person|
+
+       person.each do |name,status|
+         #binding.pry
+        if status == "Winner"
+            full_name = person["name"]
+            return full_name.split(' ').first
+        end 
+      end
+    end
 end
+
+
 
 def get_contestant_name(data, occupation)
   # code here
+   data.each do |season, array|
+     
+    array.each do |people|
+      #binding.pry
+      people.each do |key,value|
+        if value == occupation
+          return people["name"]
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  counter =0
+  data.each do |season, array|
+  array.each do |people|
+      #binding.pry
+      people.each do |key,value|
+        if value == hometown
+          counter +=1
+        end
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
   # code here
+  data.each do |season, array|
+     
+    array.each do |people|
+      #binding.pry
+      people.each do |key,value|
+        if value == hometown
+          return people["occupation"]
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+   # code here
+  age = 0
+count = 0
+  data[season].each do |person|
+   # binding.pry
+    person.each do |key,value|
+      if key == "age"
+        count += 1
+        age += value.to_f
+      end
+    end
+  end
+
+   answer = (age/count).round
+return answer
 end
