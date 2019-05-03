33c79ed32a83147fc42a84bc255413d0b378dba8
feven <feventsegay6@gmail.com>
2019-05-03 02:54:08 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,17 +1,53 @@
+#require "pry"
 def get_first_name_of_season_winner(data, season)
-  # code here
+ data[season].each do |person|
+
+       person.each do |name,status|
+         #binding.pry
+        if status == "Winner"
+            full_name = person["name"]
+            return full_name.split(' ').first
+        end 
+      end
+    end
 end
+
+
 
 def get_contestant_name(data, occupation)
   # code here
+   data.each do |season, array|
+     
+    array.each do |people|
+      #binding.pry
+      people.each do |key,value|
+        if value == occupation
+          return people["name"]
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  counter =0
+  data.each do |season, array|
+  array.each do |people|
+      #binding.pry
+      people.each do |key,value|
+        if value == hometown
+          counter +=1
+        end
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
   # code here
+  
 end
 
 def get_average_age_for_season(data, season)
